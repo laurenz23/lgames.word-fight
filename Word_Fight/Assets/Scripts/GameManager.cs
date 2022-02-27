a091f17ea84e576df855124a170e39bdc88bbe98
unknown <laurenzgonz23@gmail.com>
2022-02-27 08:55:51 +0800

    initial commit

---
+++ Word_Fight/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace LGAMES.WordFight
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        #region :: Inspector Variables
+        // add inspector variable here... 
+        // add headers for multiple inspector variables 
+        // public variables 
+        // private variables 
+        #endregion
+
+        #region :: Variables
+        // add class variable here...
+        // public variables must hide in inspector
+        // private variables 
+        #endregion
+
+        #region :: Class Reference
+        // add class reference here...
+        // [Header("Class Reference")]
+        #endregion
+
+        #region :: Listeners
+        // add listeners here...
+        #endregion
+
+        #region :: Lifecycle
+        // add class lifecycle here...
+        #endregion
+
+        #region :: Properties
+        // add class properties here...
+        // setter first
+        // getter second
+        #endregion
+
+        #region :: Events
+        // add event listener here...
+        #endregion
+
+        #region :: Functions
+        // add functions or methods here... 
+        #endregion
+
+        #region :: Enumerator 
+        // add IEnumerator here...
+        #endregion
+
+    }
+}
